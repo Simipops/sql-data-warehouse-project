a62b850a3ba2d043bcbd2c1ec28f4baba731cd37
Simipops <idrisabidemiassurance@gmail.com>
2025-11-05 06:35:25 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -14,9 +14,9 @@ Parameters:
 
 Usage Example:
     EXEC bronze.load_bronze;
+========================================================================================
 */
 
-EXEC bronze.load_bronze
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
